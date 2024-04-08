d70b12c1cae85363079c26558efcb3a099c874d4
Abel Svoboda <abel465@gmail.com>
2024-04-08 14:32:49 +1200

    Replace sierpinski triangle algorithm with loopless version

--- shaders/shared/src/sdf_2d/fractal.rs
+++ shaders/shared/src/sdf_2d/fractal.rs
@@ -1,20 +1,26 @@
 use crate::SQRT_3;
-use spirv_std::glam::{vec2, Vec2, Vec2Swizzles};
+use spirv_std::glam::{vec3, Vec2};
 #[cfg_attr(not(target_arch = "spirv"), allow(unused_imports))]
 use spirv_std::num_traits::Float;
 
-pub fn sierpinski_triangle(mut p: Vec2, mut r: f32, m: u32) -> f32 {
-    const N: Vec2 = vec2(0.5, -0.5 * SQRT_3);
-
-    let mut d = super::equilateral_triangle(p, r);
-
-    for _ in 0..m {
-        p.x = p.x.abs();
-        r *= 0.5;
-        p += N.yx() * r;
-        p -= N * N.dot(p - Vec2::Y * r).min(0.0) * 2.0;
-        d = super::equilateral_triangle(p, r);
+// Based on https://www.shadertoy.com/view/fdBcR3 (Jakob Thomsen 2/2/2022)
+pub fn sierpinski_triangle(p: Vec2, r: f32, m: u32) -> f32 {
+    let q = {
+        let p = p / r / SQRT_3;
+        let z = p.y / SQRT_3;
+        vec3(-z - p.x, -z + p.x, 2.0 * z) + 1.0 / 3.0
+    };
+    if q.x < 0.0 || q.y < 0.0 || q.z < 0.0 {
+        super::equilateral_triangle(p, r)
+    } else {
+        let n = (m as f32).exp2();
+        let i = ((1.0 - q) * n).as_ivec3();
+        let f = i.x & i.y & i.z;
+        if f == 0 {
+            -(q % (1.0 / n)).min_element() * r * 3.0 / 2.0
+        } else {
+            let s = (f as f32).log2().floor().exp2() * 2.0 / n;
+            (((1.0 - q) * 2.0) % s).min_element() * r * 3.0 / 4.0
+        }
     }
-
-    d
 }
