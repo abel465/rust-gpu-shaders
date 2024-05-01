a49f6fc76ac40f938a3e934d6db7bc60c0ba1f49
Abel Svoboda <abel465@gmail.com>
2024-05-01 13:15:00 +1200

    Enable querying distance field with mouse press for sdf builder tree shader

--- shaders/shared/src/push_constants/sdf_builder.rs
+++ shaders/shared/src/push_constants/sdf_builder.rs
@@ -1,9 +1,11 @@
-use super::Size;
+use super::{Bool, Size, Vec2};
 use bytemuck::{Pod, Zeroable};
 
 #[derive(Copy, Clone, Pod, Zeroable)]
 #[repr(C)]
 pub struct ShaderConstants {
     pub size: Size,
+    pub cursor: Vec2,
     pub time: f32,
+    pub mouse_button_pressed: Bool,
 }
